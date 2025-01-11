b3d931b59b1005cabdd1dc00dedaa9f20e7de13e
Kuraishi Taiki <131664944+taiki-kuraishi@users.noreply.github.com>
2025-01-12 00:50:44 +0900

    Add env validation middleware (#20)
    * Add environment variable validation middleware
    
    * add vitest to lefthook
    
    * remove unused usecase class
    
    * add test of validateEnvMiddleware

---
+++ test/helpers/mockEnv.ts
@@ -0,0 +1,16 @@
+import type { EnvType } from '@/Middlewares/validateEnvMiddleware';
+
+/**
+ * Mock cloudflare worker Env
+ * @param {Partial<EnvType>} overrides
+ * @returns {EnvType} mock env
+ */
+export function mockEnv(overrides: Partial<EnvType> = {}): EnvType {
+	return {
+		DISCORD_OAUTH_BASE_URL: 'https://discord-oauth-base-url',
+		DISCORD_ID: 'discord-id',
+		DISCORD_SECRET: 'discord-secret',
+		DISCORD_REDIRECT_URL: 'https://discord-redirect-url',
+		...overrides,
+	};
+}
